db7a815f408d1be486606439f9df93a300028fd4
Mike Boyle <mikesboyle@gmail.com>
2015-08-11 22:39:25 -0400

    update gitignore and change local db to postgresql to match production

--- educ8/settings.py
+++ educ8/settings.py
@@ -103,7 +103,7 @@ STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default='sqlite://db/sqlite3.db')
+DATABASES['default'] =  dj_database_url.config(default='postgres://Mike@localhost/educ8')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
