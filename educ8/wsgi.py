58b4ae73e923740d02137c3f70ae76cd605003fc
Mike Boyle <mikesboyle@gmail.com>
2015-08-11 23:42:20 -0400

    configure with whitenoise to serve static files on heroku

--- educ8/wsgi.py
+++ educ8/wsgi.py
@@ -19,3 +19,9 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
